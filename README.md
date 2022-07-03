ed291eaf5f5008e1a4bb39b355a110b0f27de4d8
Tim Gibbon <tng@chegwin.org>
2022-07-03 11:36:16 +0100

    Updated infrastructure

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # cuddly-guide
 Bianchi Accountants website
 
+```$ git submodule init
+   Submodule 'hugo/themes/hugo-hero-theme' (https://github.com/zerostaticthemes/hugo-hero-theme) registered for path 'themes/hugo-hero-theme'
+   $ git submodule update
+```
+
 
--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -1,41 +1,55 @@
 # S3 bucket for website.
-resource "aws_s3_bucket" "www_bucket" {
+resource "aws_s3_bucket" "www" {
   bucket = "www.${var.domain_name}"
-  acl    = "public-read"
-  policy = templatefile("templates/s3-policy.json", { bucket = "www.${var.domain_name}" })
+  tags = var.common_tags
+
+}
+
+resource "aws_s3_bucket_website_configuration" "www" {
+  bucket = aws_s3_bucket.www.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_cors_configuration" "www_cors" {
+  bucket = aws_s3_bucket.www.bucket
 
   cors_rule {
-    allowed_headers = ["Authorization", "Content-Length"]
-    allowed_methods = ["GET", "POST"]
+    allowed_headers = ["*"]
+    allowed_methods = ["GET"]
     allowed_origins = ["https://www.${var.domain_name}"]
-    max_age_seconds = 3000
+    expose_headers  = ["ETag"]
   }
+}
 
-  website {
-    index_document = "index.html"
-    error_document = "404.html"
-  }
-
-  tags = var.common_tags
+resource "aws_s3_bucket" "root" {
+  bucket = var.domain_name
+  policy = templatefile("templates/s3-policy.json", { bucket = var.domain_name })
 }
 
 # S3 bucket for redirecting non-www to www.
-resource "aws_s3_bucket" "root_bucket" {
-  bucket = var.domain_name
+resource "aws_s3_bucket_acl" "root" {
+  bucket = aws_s3_bucket.root.id
   acl    = "public-read"
-  policy = templatefile("templates/s3-policy.json", { bucket = var.domain_name })
-
-  website {
-    redirect_all_requests_to = "https://www.${var.domain_name}"
-  }
-
-  tags = var.common_tags
 }
 
+
+resource "aws_s3_bucket_website_configuration" "root" {
+  bucket = aws_s3_bucket.root.id
+
+  redirect_all_requests_to {
+    host_name =  "www.${var.domain_name}"
+  }
+}
 
 # Upload content
 resource "null_resource" "remove_and_upload_to_s3" {
   provisioner "local-exec" {
-    command = "aws s3 sync --metadata-directive REPLACE --cache-control 'max-age=86400, public' ${var.site_content} s3://${aws_s3_bucket.www_bucket.id}"
+    command = "aws s3 sync --metadata-directive REPLACE --cache-control 'max-age=86400, public' ${var.site_content} s3://${aws_s3_bucket.www.bucket}"
   }
 }
